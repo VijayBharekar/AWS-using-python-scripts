ae73f51af9cb17615a6480638e136fe7de6725d6
VijayBharekar <vijaybharekar10@gmail.com>
2016-07-27 11:02:18 +0530

    aws scripts to start and stop ec2 instances VB

---
+++ EC2_status.py
@@ -0,0 +1,14 @@
+checking EC2 instance status
+import boto3
+
+region='us-east-1'
+instances=['i-a1c18027']
+
+ec2=boto3.client('ec2',region_name=region)
+#ec2.stop_instances(InstanceIds=instances)
+output = ec2.describe_instance_status(InstanceIds=instances)
+#print 'your instance status is  : %s' %instances[0]
+if output["InstanceStatuses"][0]["InstanceId"] == instances[0]:
+   print ' instance  %s' %instances[0] + ' is in '+ output["InstanceStatuses"][0]["InstanceState"]["Name"] + ' mode'
+else:
+    print "Try again..please!!!"
