d26145740b804f38cb43d1b75815e434602c2816
VijayBharekar <vijaybharekar10@gmail.com>
2016-08-16 16:34:56 +0530

    Update stop_ec2.py

--- stop_ec2.py
+++ stop_ec2.py
@@ -1,7 +1,7 @@
 import boto3
 
-region='us-east-1'
-instances=['i-a1c18027']
+region='xx-xxxx-x'
+instances=['i-xxxxxxxx']
 def lambda_handler(event, context):
 	ec2=boto3.client('ec2',region_name=region)
 	ec2.stop_instances(InstanceIds=instances)
